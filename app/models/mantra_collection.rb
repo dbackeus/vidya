46460c8df093809e82d97a00d632e077e54d5495
David Backeus <duztdruid@gmail.com>
2011-02-12 13:34:31 +0100

    Validations for mantra collection and delete all empty mantras on save.

--- app/models/mantra_collection.rb
+++ app/models/mantra_collection.rb
@@ -5,5 +5,15 @@ class MantraCollection
 
   embeds_many :mantras
   
+  validates_presence_of :title
+  
   accepts_nested_attributes_for :mantras
+  
+  before_validation :clean_empty_mantras
+  
+  private
+  def clean_empty_mantras
+    mantras.each { |mantra| mantra.delete if mantra.original.blank? }
+    mantras.delete_if { |mantra| mantra.original.blank? }
+  end
 end
